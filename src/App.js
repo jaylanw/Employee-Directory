d4f615adcb7fb7c24a2a29cf6d0d1510388be9f4
Jaylan <jaylanwienckowski@gmail.com>
2020-08-28 20:03:20 -0500

    adding table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav'
+import EmployeeTable from './components/EmployeeTable'
 function App() {
   return (
     <div className="App">
       <Nav />
-     
+      <EmployeeTable />
     </div>
   );
 }
