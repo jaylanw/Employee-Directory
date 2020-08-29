538b0db0e2090c209cd6fc6c54463603177a5547
Jaylan <jaylanwienckowski@gmail.com>
2020-08-29 00:52:00 -0500

    adding populated table

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import EmployeeTable from './components/EmployeeTable'
+import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
       <Nav />
       <EmployeeTable />
+      <Container />
     </div>
   );
 }
--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -1,40 +1,16 @@
-import React from 'react';
-import './style.css'
+import React from "react";
 
-function EmployeeTable () {
-    return (
-<div className="container">
-<table className="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-</div>
-    )
+function EmployeeTable(props) {
+  return (
+      <table className="table table-hover">    
+            <td>{props.fName}</td>
+            <td>{props.lName}</td>
+            <td>{props.email}</td>
+            <td>
+              <img src={props.picture} className="img-fluid" alt="..." />
+            </td>
+
+      </table>
+  );
 }
 export default EmployeeTable;
