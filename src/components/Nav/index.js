9ef084df91c27557b5de0690d112d9d2f07534b4
Jaylan <jaylanwienckowski@gmail.com>
2020-08-28 19:29:29 -0500

    adding navbar to app

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ import './style.css'
+function Navbar () {
+    return (
+      <nav className="navbar justify-content-center">
+      <div className="navbar-brand">
+        Employee Directory
+      </div>
+    </nav>
+    )
+  }
+
+export default Navbar;
